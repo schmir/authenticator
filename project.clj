99d130d96b6339f160c0aaf33cceddd3b161c71e
Ralf Schmitt <ralf@systemexit.de>
2014-08-16 16:37:43 +0200

    add :scm tag to project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot [authenticator.impl]
+  :scm {:name "git"
+        :url "https://github.com/schmir/authenticator.git"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
