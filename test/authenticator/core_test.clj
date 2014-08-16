85edf64067ac0b866356e79070afd2159ff5fe56
Ralf Schmitt <ralf@systemexit.de>
2014-08-16 16:13:09 +0200

    import leiningen project template

---
+++ test/authenticator/core_test.clj
@@ -0,0 +1,7 @@
+(ns authenticator.core-test
+  (:require [clojure.test :refer :all]
+            [authenticator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
